28688be985bfb018a7ba0f33b4a9d1697d46840d
000itsuki000 <itsuki.h@collabo-style.co.jp>
2023-10-09 12:11:15 +0900

    Initial Commit

---
+++ gestureRecognizersDemo/gestureRecognizersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestureRecognizersDemoApp.swift
+//  gestureRecognizersDemo
+//
+//  Created by Itsuki on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct gestureRecognizersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
